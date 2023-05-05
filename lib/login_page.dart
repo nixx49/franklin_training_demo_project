b94a766cc3720514c93837272ca8074d58410cd2
Nimantha Karunanayake <nimanthakmp@gmail.com>
2023-05-05 20:41:16 +0530

    Adding logo to inside the container widget.

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -25,11 +25,20 @@ class _LogInPageState extends State<LogInPage> {
         ),
       ),
       body: Container(
-        child: Column(
-          children: [
-
-          ],
-        ),
+        padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
+        margin: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
+        decoration: BoxDecoration(
+            color: Colors.orange,
+            borderRadius: BorderRadius.circular(10),
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.grey.withOpacity(0.4),
+                  blurRadius: 2.0,
+                  spreadRadius: 3.0)
+            ]),
+        width: 200,
+        height: 200,
+        child: Image.asset('images/newlogo.png'),
       ),
     );
   }

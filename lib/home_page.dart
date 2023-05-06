7b1b1223f397132399404607834dd92fb01f08af
Nimantha Karunanayake <nimanthakmp@gmail.com>
2023-05-07 00:40:10 +0530

    Adding API data to List View.

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -17,6 +17,7 @@ class _HomePageState extends State<HomePage> {
     apiService.getUsers();
     //this is the main thread
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,12 +27,65 @@ class _HomePageState extends State<HomePage> {
       body: Container(
         child: Column(
           children: [
-            // Expanded(child: FutureBuilder(
-            //   future: ,
-            // )),
+            Expanded(
+                child: FutureBuilder(
+              future: apiService.getUsers(),
+              builder: (context, snapShot) {
+                if (snapShot.hasData) {
+                  return ListView.builder(
+                    itemCount: snapShot.data!.length,
+                    itemBuilder: (context, index) {
+                      return GestureDetector(
+                        onTap: () {},
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            boxShadow: [
+                              BoxShadow(
+                                blurRadius: 1,
+                                spreadRadius: 1,
+                                color: Colors.grey.withOpacity(0.4),
+                              ),
+                            ],
+                          ),
+                          margin: const EdgeInsets.symmetric(
+                              horizontal: 10, vertical: 10),
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 10, vertical: 10),
+                          child: ListTile(
+                            title: Text(
+                              snapShot.data![index].name ?? '',
+                              style: const TextStyle(
+                                  fontSize: 18, color: Colors.black),
+                            ),
+                            subtitle: Text(
+                              snapShot.data![index].city ?? '',
+                              style: const TextStyle(
+                                  fontSize: 18, color: Colors.black),
+                            ),
+                          ),
+                        ),
+                      );
+                      //Container(margin: EdgeInsets.all(10),height:50, width: 200, color: Colors.blue,);
+                    },
+                  );
+                } else {
+                  return Container(
+                    child: const Center(
+                      child: Text(
+                        'Loading...',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 15,
+                        ),
+                      ),
+                    ),
+                  );
+                }
+              },
+            )),
           ],
         ),
-
       ),
     );
   }

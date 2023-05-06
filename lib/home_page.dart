faf156d75bdbabd630255f9506fd551696e493d4
Nimantha Karunanayake <nimanthakmp@gmail.com>
2023-05-06 13:15:58 +0530

    Adding Separate List View

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -16,29 +16,72 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: const Text('Home Page'),
       ),
-      body: Column(
-        children: [
-          const SizedBox(
-            height: 20.0,
-          ),
-          Center(
-            child: Container(
-              padding: const EdgeInsets.symmetric(horizontal: 20.0),
-              child: Text(
-                'welcome ${widget.username}',
-                style: const TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.grey),
+      body: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        child: Column(
+
+          children: [
+            const SizedBox(
+              height: 20.0,
+            ),
+            Center(
+              child: Container(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                child: Text(
+                  'welcome ${widget.username}',
+                  style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey),
+                ),
               ),
             ),
-          ),
-          ElevatedButton(
-            onPressed: (){
-              Navigator.push(context, MaterialPageRoute(builder: (context) => const LogInPage()));
-            },child: const Text('LogOut'),
-          )
-        ],
+            Expanded(
+                child: ListView.separated(
+                  //physics: const ClampingScrollPhysics(),
+              itemCount: 20,
+              separatorBuilder: (context, index) {
+                return Container(
+                  color: Colors.grey,
+                  height: 1,
+                );
+              },
+              itemBuilder: (context, index) {
+                return Container(
+                  margin: const EdgeInsets.symmetric(vertical: 10),
+                  height: 50,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.blue,
+                  ),
+                  child: const Center(
+                    child: Text(
+                      'Test 1234',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                );
+              },
+            )),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Container(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const LogInPage()));
+                    },
+                    child: const Text('LogOut'),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
